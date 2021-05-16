421673e61b70cb7b43a123805aa08d4b09e1ae06
Chen Lijun <chenlijun1999@gmail.com>
2021-05-16 17:12:29 +0200

    feat(anki-connect): support all actions related to notes

--- packages/anki-connect/src/note.ts
+++ packages/anki-connect/src/note.ts
@@ -1,27 +1,173 @@
 import type { MediaFile } from './media';
 
+type NoteId = number;
+
 export type ActionsToPayloadMap = {
+  /**
+   * Creates a note using the given deck and model, with the provided field
+   * values and tags. Returns the identifier of the created note created on
+   * success, and null on failure.
+   */
   addNote: {
     6: {
       request: {
         note: Note;
       };
-      response: number | null;
+      response: NoteId | null;
     };
   };
+  /**
+   * Creates multiple notes using the given deck and model, with the provided
+   * field values and tags. Returns an array of identifiers of the created
+   * notes (notes that could not be created will have a null identifier). Please
+   * see the documentation for addNote for an explanation of objects in the
+   * notes array.
+   */
   addNotes: {
     6: {
       request: {
         notes: Array<Note>;
       };
-      response: Array<number | null>;
+      response: Array<NoteId | null>;
     };
   };
+  /**
+   * Accepts an array of objects which define parameters for candidate notes
+   * (see addNote) and returns an array of booleans indicating whether or not
+   * the parameters at the corresponding index could be used to create a new
+   * note.
+   */
+  canAddNotes: {
+    6: {
+      request: {
+        notes: Array<Note>;
+      };
+      response: Array<boolean>;
+    };
+  };
+  /**
+   * Modify the fields of an exist note. You can also include audio, video, or
+   * picture files which will be added to the note with an optional audio,
+   * video, or picture property. Please see the documentation for addNote for an
+   * explanation of objects in the audio, video, or picture array.
+   */
   updateNoteFields: {
     6: {
       request: {
         note: UpdateNote;
       };
+      response: null;
+    };
+  };
+  /**
+   * Adds tags to notes by note ID.
+   */
+  addTags: {
+    6: {
+      request: {
+        notes: NoteId[];
+        tags: string;
+      };
+      response: null;
+    };
+  };
+  /**
+   * Remove tags from notes by note ID.
+   */
+  removeTags: {
+    6: {
+      request: {
+        notes: NoteId[];
+        tags: string;
+      };
+      response: null;
+    };
+  };
+  /**
+   * Gets the complete list of tags for the current user.
+   */
+  getTags: {
+    6: {
+      request: void;
+      response: string[];
+    };
+  };
+  /**
+   * Clears all the unused tags in the notes for the current user.
+   */
+  clearUnusedTags: {
+    6: {
+      request: void;
+      response: null;
+    };
+  };
+  /**
+   * Replace tags in notes by note ID.
+   */
+  replaceTags: {
+    6: {
+      request: {
+        notes: NoteId[];
+        tag_to_replace: string;
+        replace_with_tag: string;
+      };
+      response: null;
+    };
+  };
+  /**
+   * Replace tags in all the notes for the current user.
+   */
+  replaceTagsInAllNotes: {
+    6: {
+      request: {
+        tag_to_replace: string;
+        replace_with_tag: string;
+      };
+      response: null;
+    };
+  };
+  /**
+   * Returns an array of note IDs for a given query. Query syntax is
+   * [documented here](https://docs.ankiweb.net/#/searching).
+   */
+  findNotes: {
+    6: {
+      request: {
+        query: string;
+      };
+      response: NoteId[];
+    };
+  };
+  /**
+   * Returns a list of objects containing for each note ID the note fields,
+   * tags, note type and the cards belonging to the note.
+   */
+  notesInfo: {
+    6: {
+      request: {
+        notes: NoteId[];
+      };
+      response: NoteInfo[];
+    };
+  };
+  /**
+   * Deletes notes with the given ids. If a note has several cards associated
+   * with it, all associated cards will be deleted.
+   */
+  deleteNotes: {
+    6: {
+      request: {
+        notes: NoteId[];
+      };
+      response: null;
+    };
+  };
+  /**
+   * Removes all the empty notes for the current user.
+   */
+  removeEmptyNotes: {
+    6: {
+      request: void;
       response: null;
     };
   };
@@ -80,4 +226,8 @@ export interface Note {
   picture?: Array<NoteMediaFile>;
 }
 
-export type UpdateNote = Omit<Note, 'deckName' | 'modelName'> & { id: number };
+export type NoteInfo = Pick<Note, 'modelName' | 'tags' | 'fields'> & {
+  id: NoteId;
+};
+
+export type UpdateNote = Omit<Note, 'deckName' | 'modelName'> & { id: NoteId };
