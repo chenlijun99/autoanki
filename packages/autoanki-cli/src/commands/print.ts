chenlijun99/autoanki

commit dd5818332064f3c5c4c062bd0178110929004b42

refactor: drastically changed how media files are handled
BREAKING CHANGE: plugins' constructor signature changed

packages/autoanki-cli/src/commands/print.ts
@@ -5,6 +5,7 @@ import type {
   SourcePlugin,
   TransformerPlugin,
 } from '@autoanki/core';
+import { getPluginName } from '@autoanki/core';
 
 import { extractAnkiNotesFromFiles } from '../utils/index.js';
 
@@ -29,7 +30,7 @@ function isAutoankiMediaFile(
 function replacer(key: string, value: any): any {
   if (isAutoankiMediaFile(value)) {
     return {
-      fromPlugin: value.fromPlugin.name,
+      fromPlugin: getPluginName(value.fromPlugin),
       media: {
         filename: value.media.filename,
         content: '[Media content not shown...]',
@@ -37,7 +38,7 @@ function replacer(key: string, value: any): any {
     };
   }
   if (isPlugin(value)) {
-    return value.name;
+    return getPluginName(value);
   }
   return value;
 }

.eslintrc.js
@@ -109,5 +109,9 @@ module.exports = {
     'unicorn/prefer-spread': 'off',
     'unicorn/no-empty-file': 'off',
     'unicorn/prefer-ternary': ['warn', 'only-single-line'],
+    /**
+     * We already have TypeScript to check for us
+     */
+    'import/named': 'off',
   },
 };

jest.config.cjs
@@ -18,8 +18,12 @@ function projectConfig(packageName) {
       ...tsconfigPathMaps,
       '^@autoanki/utils/webcrypto\\.js$':
         '<rootDir>/packages//autoanki-utils/src/webcrypto.node',
+      '^@autoanki/utils/hash-sync\\.js$':
+        '<rootDir>/packages//autoanki-utils/src/hash-sync.node',
       '^@autoanki/utils/(.*)\\.js$':
         '<rootDir>/packages//autoanki-utils/src/$1',
+      '^#webcrypto\\.js$':
+        '<rootDir>/packages//autoanki-utils/src/webcrypto.node',
       '^(\\.{1,2}/.*)\\.js$': '$1',
     },
     transform: {
@@ -43,5 +47,9 @@ function projectConfig(packageName) {
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  projects: [projectConfig('autoanki-sync')],
+  projects: [
+    projectConfig('autoanki-sync'),
+    projectConfig('autoanki-core'),
+    projectConfig('autoanki-utils'),
+  ],
 };

packages/autoanki-cli/src/commands/sync/index.ts
@@ -2,7 +2,10 @@ import { writeFile } from 'node:fs/promises';
 
 import type { CommandModule } from 'yargs';
 
-import { groupAutoankiNotesBySourcePluginAndInput } from '@autoanki/core';
+import {
+  groupAutoankiNotesBySourcePluginAndInput,
+  getPluginName,
+} from '@autoanki/core';
 import syncPlugin, {
   AutomaticSyncAction,
   SyncAction,
@@ -33,7 +36,7 @@ function syncActionToString(action: SyncAction) {
     str += 'Create notes in Anki\n';
     const grouped = groupAutoankiNotesBySourcePluginAndInput(action.newNotes);
     for (const [sourcePlugin, groupedbySource] of grouped) {
-      str += `      From "${sourcePlugin.name}"\n`;
+      str += `      From "${getPluginName(sourcePlugin)}"\n`;
       for (const [source, notes] of groupedbySource) {
         str += `         ${notes.length} new notes from "${source.key}"\n`;
       }
@@ -45,7 +48,7 @@ function syncActionToString(action: SyncAction) {
       (item) => item.note.fromSource
     );
     for (const [sourcePlugin, groupedbySource] of grouped) {
-      str += `      From "${sourcePlugin.name}"\n`;
+      str += `      From "${getPluginName(sourcePlugin)}"\n`;
       for (const [source, notes] of groupedbySource) {
         str += `         from "${source.key}"\n`;
         for (const note of notes) {
@@ -60,7 +63,7 @@ function syncActionToString(action: SyncAction) {
       (item) => item.note.fromSource
     );
     for (const [sourcePlugin, groupedbySource] of grouped) {
-      str += `      From "${sourcePlugin.name}"\n`;
+      str += `      From "${getPluginName(sourcePlugin)}"\n`;
       for (const [source, notes] of groupedbySource) {
         str += `         from "${source.key}"\n`;
         for (const note of notes) {
@@ -74,7 +77,7 @@ function syncActionToString(action: SyncAction) {
       action.notesToBeRemoved
     );
     for (const [sourcePlugin, groupedbySource] of grouped) {
-      str += `      From "${sourcePlugin.name}"\n`;
+      str += `      From "${getPluginName(sourcePlugin)}"\n`;
       for (const [source, notes] of groupedbySource) {
         str += `         from "${source.key}"\n`;
         for (const note of notes) {
@@ -89,7 +92,7 @@ function syncActionToString(action: SyncAction) {
       (item) => item.note.fromSource
     );
     for (const [sourcePlugin, groupedbySource] of grouped) {
-      str += `      From "${sourcePlugin.name}"\n`;
+      str += `      From "${getPluginName(sourcePlugin)}"\n`;
       for (const [source, notes] of groupedbySource) {
         str += `         from "${source.key}"\n`;
         for (const note of notes) {

packages/autoanki-core/jest.config.ts
@@ -1,9 +1,0 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
-  verbose: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
-
-export default config;

packages/autoanki-core/src/index.ts
@@ -1,12 +1,16 @@
 export { configSchema } from './config.js';
 export type { Config, ConfigPluginInstance } from './config.js';
+
 export type {
   AutoankiPlugin,
+  AutoankiPluginApi,
   SourcePlugin,
   TransformerPlugin,
   SourcePluginParsingOutput,
   TransformerPluginOutput,
 } from './plugin.js';
+export { getPluginName } from './plugin.js';
+
 export {
   extractAutoankiNotes,
   groupAutoankiNotesBySourcePluginAndInput,
@@ -17,9 +21,21 @@ export {
 } from './notes.js';
 export type {
   AutoankiNote,
-  AutoankiMediaFile,
   NoteInput,
   NoteInputs,
   LazyNoteInputs,
   ParsedNote,
 } from './notes.js';
+
+export {
+  computeMediaFileMetadataSync,
+  computeMediaFileMetadata,
+  computeAutoankiMediaFileFromRaw,
+  computeAutoankiMediaFileFromRawSync,
+  parseMediaFileMetadataFromFilename,
+} from './media.js';
+export type {
+  RawAutoankiMediaFile,
+  AutoankiMediaFile,
+  AutoankiMediaFileMetadata,
+} from './media.js';

packages/autoanki-core/src/media.spec.ts
@@ -1,9 +1,10 @@
 import { test, fc } from '@fast-check/jest';
 
 import {
-  computeMediaFileMetadataFromMediaFile,
-  parseMediaFileMetadataDataFromFilename,
+  computeMediaFileMetadata,
+  parseMediaFileMetadataFromFilename,
   ANKI_MAX_FILENAME_LENGTH,
+  computeMediaFileMetadataSync,
 } from './media.js';
 
 describe('Metadata encoding/decoding into stored filename', () => {
@@ -16,11 +17,8 @@ describe('Metadata encoding/decoding into stored filename', () => {
   test.prop([arbitraryPluginName, arbitraryMediaFile])(
     'Metadata is properly encoded in the filename and can be retrieved from filename',
     async (pluginName, mediaFile) => {
-      const metadata = await computeMediaFileMetadataFromMediaFile(
-        pluginName,
-        mediaFile
-      );
-      const metadataFromFileName = parseMediaFileMetadataDataFromFilename(
+      const metadata = await computeMediaFileMetadata(pluginName, mediaFile);
+      const metadataFromFileName = parseMediaFileMetadataFromFilename(
         metadata.storedFilename
       );
       return expect(metadataFromFileName).toEqual(metadata);
@@ -30,10 +28,7 @@ describe('Metadata encoding/decoding into stored filename', () => {
   test.prop([arbitraryPluginName, arbitraryMediaFile])(
     'Encoded filename length is <= than Anki limits',
     async (pluginName, mediaFile) => {
-      const metadata = await computeMediaFileMetadataFromMediaFile(
-        pluginName,
-        mediaFile
-      );
+      const metadata = await computeMediaFileMetadata(pluginName, mediaFile);
 
       expect(metadata.storedFilename.length).toBeLessThanOrEqual(
         ANKI_MAX_FILENAME_LENGTH
@@ -48,12 +43,19 @@ describe('Metadata encoding/decoding into stored filename', () => {
         // artifically insert underscore
         mediaFile.filename = '_' + mediaFile.filename;
       }
-      const metadata = await computeMediaFileMetadataFromMediaFile(
-        pluginName,
-        mediaFile
-      );
+      const metadata = await computeMediaFileMetadata(pluginName, mediaFile);
 
       expect(metadata.storedFilename[0]).toBe('_');
+    }
+  );
+
+  test.prop([arbitraryPluginName, arbitraryMediaFile])(
+    'Sync and async version produced the same output',
+    async (pluginName, mediaFile) => {
+      const metadata = await computeMediaFileMetadata(pluginName, mediaFile);
+      const metadataSync = computeMediaFileMetadataSync(pluginName, mediaFile);
+
+      expect(metadataSync).toEqual(metadata);
     }
   );
 });

packages/autoanki-core/src/media.ts
@@ -1,7 +1,10 @@
-import type { AutoankiMediaFile } from '@autoanki/core';
+/**
+ * @file Media files related logic
+ */
+import { z } from 'zod';
 import assert from '@autoanki/utils/assert.js';
-
-import { hashContent } from './hash.js';
+import { hashContent } from '@autoanki/utils/hash.js';
+import { hashContentSync } from '@autoanki/utils/hash-sync.js';
 
 /**
  * See https://docs.ankiweb.net/templates/styling.html#installing-fonts
@@ -23,33 +26,27 @@ const ACTUAL_FILENAME_MAX_LENGTH =
   // Initial underscore, which may be included if actual filename starts with underscore
   1;
 
+export const autoankiMediaFileMetadataSchema = z
+  .object({
+    /**
+     * The complete filename as it is stored in Anki
+     */
+    storedFilename: z.string(),
+    /**
+     * Digest of the media file
+     */
+    digest: z.string(),
+  })
+  .strict();
+
 /**
  * Metadata of an Autoanki media file.
  *
  * It is fully encoded inside the media file's filename stored in Anki.
  */
-export interface MediaFileMetadata {
-  /**
-   * The complete filename as it is stored in Anki
-   */
-  storedFilename: string;
-  /**
-   * The true filename of the media file, comprised of name of plugin from
-   * which the media originated + filename of the media itself.
-   *
-   * It shouldn't be useful in any way for Autoanki...
-   * It may also be truncated due to filename length limit imposed by Anki.
-   * See https://github.com/ankitects/anki/blob/68fa661b532ef965e72357c2876636271f01fa67/rslib/src/media/files.rs#L24-L28
-   *
-   * But anyway, keeping the (even truncated) actual filename is useful for the
-   * user to have an idea of media file.
-   */
-  truncatedActualFilename: string;
-  /**
-   * Digest of the media file
-   */
-  digest: string;
-}
+export type AutoankiMediaFileMetadata = z.infer<
+  typeof autoankiMediaFileMetadataSchema
+>;
 
 /**
  * Truncate the filename trying to keep the extension and at least one
@@ -85,24 +82,24 @@ var filenameMetadataPrefixRegex = new RegExp(
 /**
  * Returns undefined if the filename is not valid
  */
-export function parseMediaFileMetadataDataFromFilename(
+export function parseMediaFileMetadataFromFilename(
   filename: string
-): MediaFileMetadata | undefined {
+): AutoankiMediaFileMetadata | undefined {
   const decoded = decodeURIComponent(filename);
   const match = filenameMetadataPrefixRegex.exec(decoded);
   if (match) {
     return {
       storedFilename: filename,
       digest: match[2],
-      truncatedActualFilename: match[3],
     };
   }
 }
 
-export async function computeMediaFileMetadataFromMediaFile(
+function computeMediaFileMetadataHelper(
   fromPluginName: string,
-  mediaFile: AutoankiMediaFile
-): Promise<MediaFileMetadata> {
+  mediaFile: RawAutoankiMediaFile,
+  mediaFileDigest: string
+): AutoankiMediaFileMetadata {
   assert(fromPluginName.length > 0);
   assert(mediaFile.filename.length > 0);
 
@@ -111,9 +108,6 @@ export async function computeMediaFileMetadataFromMediaFile(
     hasSpecialPrefix = true;
   }
 
-  const digest = await hashContent(mediaFile.base64Content);
-
-  // strip from input plugin name and filename
   let truncatedActualFilename = stripUnmatchedSurrogates(
     `${fromPluginName}_${mediaFile.filename}`
   );
@@ -125,7 +119,7 @@ export async function computeMediaFileMetadataFromMediaFile(
   const encodedFilename = encodeURIComponent(
     `${
       hasSpecialPrefix ? ANKI_DONT_REFERENCE_COUNT_PREFIX : ''
-    }${AUTOANKI_MEDIA_PREFIX}_${digest}_${truncatedActualFilename}`
+    }${AUTOANKI_MEDIA_PREFIX}_${mediaFileDigest}_${truncatedActualFilename}`
   );
 
   assert(
@@ -134,7 +128,64 @@ export async function computeMediaFileMetadataFromMediaFile(
   );
   return {
     storedFilename: encodedFilename,
-    truncatedActualFilename,
-    digest,
+    digest: mediaFileDigest,
   };
 }
+
+export function computeMediaFileMetadataSync(
+  fromPluginName: string,
+  mediaFile: RawAutoankiMediaFile
+): AutoankiMediaFileMetadata {
+  return computeMediaFileMetadataHelper(
+    fromPluginName,
+    mediaFile,
+    hashContentSync(mediaFile.base64Content)
+  );
+}
+
+export async function computeMediaFileMetadata(
+  fromPluginName: string,
+  mediaFile: RawAutoankiMediaFile
+): Promise<AutoankiMediaFileMetadata> {
+  return computeMediaFileMetadataHelper(
+    fromPluginName,
+    mediaFile,
+    await hashContent(mediaFile.base64Content)
+  );
+}
+
+export async function computeAutoankiMediaFileFromRaw(
+  fromPluginName: string,
+  mediaFile: RawAutoankiMediaFile
+): Promise<AutoankiMediaFile> {
+  return {
+    ...mediaFile,
+    metadata: await computeMediaFileMetadata(fromPluginName, mediaFile),
+  };
+}
+
+export function computeAutoankiMediaFileFromRawSync(
+  fromPluginName: string,
+  mediaFile: RawAutoankiMediaFile
+): AutoankiMediaFile {
+  return {
+    ...mediaFile,
+    metadata: computeMediaFileMetadataSync(fromPluginName, mediaFile),
+  };
+}
+
+export const rawAutoankiMediaFileSchema = z
+  .object({
+    filename: z.string(),
+    base64Content: z.string(),
+  })
+  .strict();
+export type RawAutoankiMediaFile = z.infer<typeof rawAutoankiMediaFileSchema>;
+
+export const autoankiMediaFileSchema = rawAutoankiMediaFileSchema
+  .extend({
+    metadata: autoankiMediaFileMetadataSchema,
+  })
+  .strict();
+
+export type AutoankiMediaFile = z.infer<typeof autoankiMediaFileSchema>;

commit 37364ef94f9de60e5eefcfc9b40d9fecd1a1a329

fix: cli `print` command was broken after dd5818332064f3c5c4c062bd0178110929004b42

packages/autoanki-cli/src/commands/print.ts
@@ -1,11 +1,7 @@
 import type { CommandModule } from 'yargs';
 
-import type {
-  AutoankiNote,
-  SourcePlugin,
-  TransformerPlugin,
-} from '@autoanki/core';
-import { getPluginName } from '@autoanki/core';
+import type { AutoankiMediaFile } from '@autoanki/core';
+import { getPluginName, isPlugin } from '@autoanki/core';
 
 import { extractAnkiNotesFromFiles } from '../utils/index.js';
 
@@ -13,28 +9,16 @@ interface Args {
   inputs: string[];
 }
 
-function isPlugin(value: any): value is SourcePlugin | TransformerPlugin {
-  if (value && value.name) {
-    const v = value as SourcePlugin & TransformerPlugin;
-    return !!(v.parseFromInput || v.writeBackToInput || v.transform);
-  }
-  return false;
-}
-
-function isAutoankiMediaFile(
-  value: any
-): value is AutoankiNote['mediaFiles'][number] {
-  return value && value.fromPlugin && isPlugin(value.fromPlugin);
+function isAutoankiMediaFile(value: any): value is AutoankiMediaFile {
+  return value && value.filename && value.base64Content;
 }
 
 function replacer(key: string, value: any): any {
   if (isAutoankiMediaFile(value)) {
     return {
-      fromPlugin: getPluginName(value.fromPlugin),
-      media: {
-        filename: value.media.filename,
-        content: '[Media content not shown...]',
-      },
+      filename: value.filename,
+      base64Content: '[Media content not shown...]',
+      metadata: value.metadata,
     };
   }
   if (isPlugin(value)) {