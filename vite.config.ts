7bc20f176051b8c34c6f92e9ebe14bfb847fa57f
Lijun Chen <chenlijun1999@gmail.com>
2022-11-21 21:54:38 +0100

    infra: introduce storybook to better develop Anki bridge scripts

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+/*
+ * We use vite for storybook
+ */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
