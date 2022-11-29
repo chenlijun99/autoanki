7837ba53fdab8e3a248c6acbef57e005a3c1eda5
Lijun Chen <chenlijun1999@gmail.com>
2022-11-29 18:28:25 +0100

    fix(cli): handle exception
    Not handling the exception causes Node.js to also print the location
    (line) where the exception happened, which is a lot of output when the
    source is minified.

--- packages/autoanki-cli/src/index.ts
+++ packages/autoanki-cli/src/index.ts
@@ -12,48 +12,54 @@ export interface GlobalArgs {
   verbose: boolean;
 }
 
-const argv = yargs(process.argv.slice(2))
-  .scriptName('autoanki')
-  .strict()
-  .command(printCommand)
-  .command(syncCommand)
-  .options({
-    verbose: {
-      description: 'Verbose output',
-      type: 'boolean',
-      alias: ['v'],
-      default: false,
-      global: true,
-    },
-  })
-  .demandCommand()
-  .recommendCommands()
-  .help()
-  .version()
-  .middleware([
-    (aArgv: GlobalArgs) => {
-      initLog(aArgv.verbose);
-    },
-    initConfig,
-  ])
-  .showHelpOnFail(false)
-  .fail((msg, error, aYargs) => {
-    if (error === undefined) {
-      /*
-       * Didn't find it in the documentation. But when error is undefined
-       * it meanas it was yargs who raised the error because the CLI
-       * wasn't well formatted.
-       * Only in that case we want to print the help.
-       */
-      console.info(msg);
-      aYargs.showHelp();
-    }
-  }).argv;
+try {
+  const argv = yargs(process.argv.slice(2))
+    .scriptName('autoanki')
+    .strict()
+    .command(printCommand)
+    .command(syncCommand)
+    .options({
+      verbose: {
+        description: 'Verbose output',
+        type: 'boolean',
+        alias: ['v'],
+        default: false,
+        global: true,
+      },
+    })
+    .demandCommand()
+    .recommendCommands()
+    .help()
+    .version()
+    .middleware([
+      (aArgv: GlobalArgs) => {
+        initLog(aArgv.verbose);
+      },
+      initConfig,
+    ])
+    .showHelpOnFail(false)
+    .fail((msg, error, aYargs) => {
+      if (error === undefined) {
+        /*
+         * Didn't find it in the documentation. But when error is undefined
+         * it meanas it was yargs who raised the error because the CLI
+         * wasn't well formatted.
+         * Only in that case we want to print the help.
+         */
+        console.info(msg);
+        aYargs.showHelp();
+      }
+    }).argv;
 
-Promise.resolve(argv)
-  .then(() => {
-    closeReadline();
-  })
-  .catch((error) => {
-    throw error;
-  });
+  Promise.resolve(argv)
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      closeReadline();
+    });
+} catch (error) {
+  console.error(error);
+} finally {
+  closeReadline();
+}
