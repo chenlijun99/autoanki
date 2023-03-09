6505776ca14f04ed26f50aaa1af893f0ed2b09ec
Lijun Chen <chenlijun1999@gmail.com>
2023-03-09 09:51:55 +0100

    infra: use Nix flake for reproducible development environment

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Autoanki development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs-16_x
+          pkgs.nodePackages.pnpm
+        ];
+      };
+    });
+}
